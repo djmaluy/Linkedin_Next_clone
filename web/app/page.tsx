286bbb07e21f7af5bbd6aae7f661229eb8b76aa0
djmaluy <a.tsvirko@ukr.net>
2025-08-07 19:00:49 +0300

    init

---
+++ web/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="">
+      <h1>sjkhfkhjs hfsafhskj</h1>
+      <Button>Click mee</Button>
+    </main>
+  );
+}
---
+++ web/components/Header.tsx
@@ -0,0 +1,28 @@
+import { SearchIcon } from "lucide-react";
+import Image from "next/image";
+
+function Header() {
+  return (
+    <div className="flex items-center p-2 max-w-6xl mx-auto">
+      <Image
+        className="rounded-lg"
+        src="/images/logo.png"
+        alt="Logo"
+        width={40}
+        height={40}
+      />
+      <div className="flex-1">
+        <form className="flex items-center space-x-1 bg-gray-100 p-2 rounded-md flex-1 mx-2 max-w-96">
+          <SearchIcon className="h-4 text-gray-600" />
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent flex-1 outline-none"
+          />
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
